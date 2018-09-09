083fd5331a87e311c4b61bf87e827746a7b461d8
MkoniecOfficial <38956561+MkoniecOfficial@users.noreply.github.com>
2018-09-09 21:26:19 +0200

    Update LUL_BOT.py

--- LUL_BOT.py
+++ LUL_BOT.py
@@ -17,5 +17,18 @@ async def on_ready():
 async def on_message(message):
     if message.content == "cookie":
         await client.send_message(message.channel, ":cookie:") #responds with Cookie emoji when someone says "cookie"
-
-client.run("NDE1NTgwMDU0NzgxNjg5ODU3.DcyEPA.prNM2x4b92gmEmYZYo2ElH5aPso") #Replace token with your bots token
+@client.event
+async def on_message(message):
+    if message.content == "Twitch":
+        await client.send_message(message.channel, "https://www.twitch.tv/mkoniec") 
+@client.event
+async def on_message(message):
+    if message.content == "YT":
+        await client.send_message(message.channel, "https://www.youtube.com/channel/UCSpXYRrwGCLCWQMqtSjmbzA") 
+		
+@client.event
+async def on_message(message):
+    if message.content == "GR":
+        await client.send_message(message.channel, "@here gracie ?") 
+		
+client.run("NDg4MDE0MjI5Mzk2Mzg5ODkw.DncBLw.95dWohkQpKfq9iKHlZ-0_TWSZZE") #Replace token with your bots token
